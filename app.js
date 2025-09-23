3e877200d915c610aad6b486ea045617720e38b4
rushin-gh <rushin.mailme@gmail.com>
2025-09-24 00:50:09 +0530

    Big html elements using React.createElement

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const heading = React.createElement("h1", { id: "heading" }, "Hello from react!");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello from react!"
+);
+
+const complexElement = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    "Child",
+    React.createElement("h1", { id: "grandchild" }, "Grandchild"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    "Child",
+    React.createElement("h1", { id: "grandchild" }, "Grandchild"),
+  ]),
+]);
+console.log(complexElement);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(complexElement);
