fda0679ee4431a481e8435887295a6179a78ad64
rushin-gh <rushin.mailme@gmail.com>
2025-09-26 00:45:37 +0530

    Installing react and react-dom locally

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Hello from react!"
 );
 
-const complexElement = React.createElement("div", { id: "parent" }, [
+const complexElement = React.createElement(
+  "div",
+  { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    "Child",
-    React.createElement("h1", { id: "grandchild" }, "Grandchild"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    "Child",
-    React.createElement("h1", { id: "grandchild" }, "Grandchild"),
-  ]),
-]);
-console.log(complexElement);
+    React.createElement("h1", { id: "grandchild", key: 1 }, "Grandchild"),
+  ])
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(complexElement);
